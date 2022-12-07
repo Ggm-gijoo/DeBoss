d217967f526ea511b9b3a0f6743d9c22acac69d0
Ggm-gijoo <gijoo1012@gmail.com>
2022-12-07 18:49:36 +0900

    특대검 올려치기 VFX

--- Assets/01_Scripts/Define/CharacterTrail.cs
+++ Assets/01_Scripts/Define/CharacterTrail.cs
@@ -13,20 +13,23 @@ public class CharacterTrail : MonoBehaviour
     public Transform instantiatePos;
     [Header("메시 셰이더")]
     public Material mat;
+    [Header("메시 부모")]
+    public Transform parentTrs;
 
     private bool isTrailActive = false;
     private SkinnedMeshRenderer[] skinnedMeshRenderers;
+
 
     public void OnTrail(float activeTime = 2f)
     {
         if (!isTrailActive)
         {
             isTrailActive = true;
-            StartCoroutine(StartTrail(activeTime));
+            StartCoroutine(StartTrail(activeTime, 10));
         }
     }
 
-    private IEnumerator StartTrail(float activeTime)
+    private IEnumerator StartTrail(float activeTime,int count)
     {
         while(activeTime > 0)
         {
@@ -35,10 +38,11 @@ public class CharacterTrail : MonoBehaviour
             if (skinnedMeshRenderers == null)
                 skinnedMeshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
 
-            foreach(var obj in skinnedMeshRenderers)
+            foreach (var obj in skinnedMeshRenderers)
             {
                 GameObject gObj = new GameObject();
                 gObj.transform.SetPositionAndRotation(instantiatePos.position, instantiatePos.rotation);
+                gObj.transform.SetParent(parentTrs);
 
                 MeshRenderer renderer = gObj.AddComponent<MeshRenderer>();
                 MeshFilter filter = gObj.AddComponent<MeshFilter>();
@@ -51,7 +55,7 @@ public class CharacterTrail : MonoBehaviour
 
                 Destroy(gObj, meshDestroyDelay);
             }
-
+                
             yield return new WaitForSeconds(meshRefreshDelay);
         }
 
